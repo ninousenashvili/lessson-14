63bf5b5df131ad4609b6793c1a7f918f7252e073
Nino <101824874+ninousenashvili@users.noreply.github.com>
2022-04-23 10:44:41 -0700

    using array method

---
+++ script.js
@@ -0,0 +1,109 @@
+// Task 1
+
+// let array = [5, 25, 89, 120, 36]
+// array.push('javascript', 'python')
+// console.log(array);
+
+// array.unshift('html', 'css');
+// console.log(array);
+
+// let length = array.length;
+// console.log(length);
+
+// array.shift();
+// console.log(array)
+
+// array.pop();
+// console.log(array);
+
+// array.splice(2, 0, 'orange');
+// console.log(array);
+
+// Task 2
+
+// let array = ['orange', 'banana', 'pear']
+// let length = array.length;
+// console.log(length);
+
+// array.push("apple", 'pineapple');
+// console.log(array);
+
+// array.unshift('watermelon');
+// console.log(array);
+
+
+// array.splice(2, 0, 'mango');
+// console.log(array);
+
+// array.shift();
+// array.pop();
+// let length = array.length;
+// console.log(length);
+
+
+
+// Task 3
+
+// let array = [12, 25, 3, 6, 8, 14, 7, 23];
+
+// let result = array.map(function (x) {
+//     return x / 3;
+// })
+
+// let result = array.map(x => x / 3);
+
+// console.log(result);
+
+
+// // task4
+// let array = ["hello", 125, "javascript", "html", 43, "css", "scss", "bootstrap", 88, 59, "python"];
+
+// let result = array.filter(x => typeof x === 'number');
+// console.log(result);
+
+
+// // Task 5
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+// let result = languages.filter(x => x.length > 3);
+// console.log(result);
+
+
+
+// // task 6
+
+// let array = ['academy', 'of', 'digital', 'industries'].reduce(function (accumulator, currentValue) {
+//     return accumulator.concat(currentValue);
+
+// },
+//     ''
+
+// )
+// console.log(array);
+
+
+// Task 8
+
+// let words = ['Madrid', 'Rome', 'Milan', 'Berlin'];
+// let result = words.filter(x => x.includes('m') || x.includes('M'));
+// console.log(result);
+
+// Task 7
+
+let item = [12, 'google', 32, null, 'yahoo', 25];
+let result = item.map(function (x) {
+    if (typeof (x) === 'number') {
+
+        return x * x;
+
+    } else if (typeof (x) === 'string') {
+        return x.toUpperCase();
+
+    } else {
+        return x;
+    }
+})
+console.log(result);
+
+
+
